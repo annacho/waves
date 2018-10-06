5e71d5db518a5e271ce156864a1f9b82440b122b
annacho <annacho13@gmail.com>
2018-10-05 21:24:45 -0700

    Add server side code and start client side code

--- server/models/user.js
+++ server/models/user.js
@@ -84,7 +84,7 @@ userSchema.statics.findByToken = function(token,cb){
   var user = this;
 
   jwt.verify(token,process.env.SECRET,function(err,decode){
-    user.findOne("_id":decode,"token":token},function(err,user){
+    user.findOne({"_id":decode,"token":token},function(err,user){
       if(err) return cb(err);
       cb(null,user)
     })
